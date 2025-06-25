9008260958fb0bd3a015d75a57a169bde44b1e72
Sean Eric Knight <seanericknight@hotmail.com>
2025-06-25 10:45:34 -0500

    Create unraid-secfix.page.php

---
+++ unraid-secfix.page.php
@@ -0,0 +1,14 @@
+<?php
+$docroot = $docroot ?? $_SERVER['DOCUMENT_ROOT'] ?: '/usr/local/emhttp';
+require_once "$docroot/webGui/include/Helpers.php";
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Unraid Security</title>
+</head>
+<body>
+  <h2>Unraid Security</h2>
+  <p>This is a working plugin page. If you see this, it’s fixed.</p>
+</body>
+</html>
