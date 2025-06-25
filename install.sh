7a66d7fa43ab7571010b33e19dc37793e759eab4
Sean Eric Knight <seanericknight@hotmail.com>
2025-06-24 21:18:47 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+PLUGIN_NAME="unraid-security"
+PLUGIN_DIR="/usr/local/emhttp/plugins/$PLUGIN_NAME"
+CONFIG_DIR="/boot/config/plugins/$PLUGIN_NAME"
+
+# Create directories
+mkdir -p "$PLUGIN_DIR"
+mkdir -p "$CONFIG_DIR"
+
+# Copy plugin files
+cp -r files/* "$PLUGIN_DIR/"
+
+# Set permissions
+chmod +x "$PLUGIN_DIR/security-checks.sh"
