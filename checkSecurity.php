cb08a6a2b52d6dbb7e57ab626751a7dbd03f10f0
Sean Eric Knight <seanericknight@hotmail.com>
2025-06-25 10:50:19 -0500

    Delete checkSecurity.php

--- checkSecurity.php
+++
@@ -1,9 +0,0 @@
-<?php
-$logfile = "/tmp/unraid-secfix.log";
-
-// Simple test output
-echo "✅ Unraid SecFix checkSecurity.php is working.\n";
-
-// Write to log file
-file_put_contents($logfile, "[" . date('Y-m-d H:i:s') . "] Security check ran.\n", FILE_APPEND);
-?>
